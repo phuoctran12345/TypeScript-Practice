74415b9996983789f878a27e711b0edd0a8ebccb
PhuocTH.dev <de180577tranhongphuoc@gmail.com>
2025-11-09 23:39:07 +0700

    lession 6

---
+++ chapter3/improve/hello.js
@@ -0,0 +1,5 @@
+function greet(name) {
+    return "Hello ".concat(name);
+}
+var msg = greet("Phước");
+console.log(msg);
---
+++ chapter3/improve/hello.ts
@@ -0,0 +1,6 @@
+function greet(name: string ): string {
+    return `Hello ${name}`;
+}
+
+const msg: string = greet("Phước");
+console.log(msg);
